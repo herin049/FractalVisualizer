c0012a6534c09cf8e25791f9956bbc24fb6cd4d8
herin049 <1019lukas@gmail.com>
2021-08-03 20:50:30 -0500

    Update some styling, add tooltips and link to github page

--- src/components/Sidebuttons.jsx
+++ src/components/Sidebuttons.jsx
@@ -6,21 +6,31 @@ const Sidebuttons = () => {
         <>
             <div className="side-button-container">
                 <button className="side-button" type="button">
-                    <i className="fas fa-cog" />
+                    <span className="material-icons">settings</span>
+                    <div className="info-tooltip">Settings</div>
                 </button>
-                <button className="side-button" type="button">
-                    <i className="fab fa-github" />
-                </button>
+                <a
+                    className="side-button"
+                    href="https://github.com/herin049/FractalVisualizer"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <span className="material-icons">code</span>
+                    <div className="info-tooltip">View the source!</div>
+                </a>
                 <div className="zoom-container">
                     <button id="zoom-in-button" type="button">
-                        <i className="fas fa-plus" />
+                        <span className="material-icons">add</span>
+                        <div className="info-tooltip">Zoom in</div>
                     </button>
                     <button id="zoom-out-button" type="button">
-                        <i className="fas fa-minus" />
+                        <span className="material-icons">remove</span>
+                        <div className="info-tooltip">Zoom out</div>
                     </button>
                 </div>
                 <button className="side-button" type="button">
-                    <i className="fas fa-redo-alt" />
+                    <span className="material-icons">restart_alt</span>
+                    <div className="info-tooltip">Reset orientation</div>
                 </button>
             </div>
             <Settings />
